3a67bd2ae7abf29b13b7fa17b429814bfad42484
kobbled <matt@groupsixtech.com>
2023-05-08 18:51:17 -0600

    update relative file search scripts

--- scripts/getRelFileLink.js
+++ scripts/getRelFileLink.js
@@ -1,5 +1,5 @@
 const getCurrFolder = require(app.vault.adapter.getBasePath() + "/_scripts/getCurrFolder.js");
 function getRelFileLink(currPath, parent_level, alias, path) {
-    return(`[${alias}](file:///${getCurrFolder(currPath, parent_level)}/${path})`)
+    return(`[${alias}](file:///${getCurrFolder(currPath, parent_level)}/${path.replaceAll(/\s+/g, "%20")})`)
 }
 module.exports = getRelFileLink;
